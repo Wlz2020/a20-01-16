993765e0aaf994b4507046bd1a06732e7eec6d95
wlz <2667461198@qq.com>
2020-01-16 08:15:31 +0800

    first git

---
+++ app/src/main/java/com/example/wlz1_16/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wlz1_16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
